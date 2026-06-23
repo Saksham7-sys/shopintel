e3d8fe8b59c336cb8e23bc235c9c1bf816a0523b
Saksham Thakur <sakshamthakur137@gmail.com>
2026-06-23 22:47:57 +0530

    Allow deployed frontend origin in backend CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -22,7 +22,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://your-frontend-name.vercel.app",   # replace after Vercel deploy
+        "https://shopintel-livid.vercel.app/",   # replace after Vercel deploy
     ],
     allow_credentials=True,
     allow_methods=["*"],
